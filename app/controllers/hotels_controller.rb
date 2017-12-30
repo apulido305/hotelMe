b12b10ad6d12ce80b046a15c074b3bd22ac8d75f
Antonio Pulido <rhema305@gmail.com>
2017-12-30 11:30:54 -0500

    added hotels model, with migration, controller, and routes.

---
+++ app/controllers/hotels_controller.rb
@@ -0,0 +1,8 @@
+class HotelsController < ApplicationController
+# index action that delivers a list of hotels
+  def index # in rails instance methods in controllers
+            # are called actions
+
+  end
+
+end
---
+++ db/migrate/20171230162049_create_hotels.rb
@@ -0,0 +1,11 @@
+class CreateHotels < ActiveRecord::Migration[5.1]
+  def change
+    create_table :hotels do |t|
+      t.integer :room_count
+      t.string :location
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
